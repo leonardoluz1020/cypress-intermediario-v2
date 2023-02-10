365cfb655878e4a018af8346804466464c7b4d00
Leonardo Luz <leonardoluz10@hotmail.com>
2023-02-10 13:41:44 -0300

    Create Project

---
+++ cypress/e2e/gui/createProject.cy.js
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker'
+
+describe('Create Project', () => {
+    beforeEach(() => {
+        cy.login();
+    })
+
+    it('successfully', () => {
+        const projet = {
+            name: `project-${faker.datatype.uuid()}`,
+            description: faker.random.words(5)
+        }
+        cy.gui_createProject(projet)
+
+        cy.url().should('be.equal', `${Cypress.config('baseUrl')}/${Cypress.env('user_name')}/${projet.name}`)
+        cy.contains(projet.name).should('be.visible')
+        cy.contains(projet.description).should('be.visible')
+
+    })
+})

4523978f9a1737be57c565e11174d36cd087dd99
Leonardo Luz <leonardoluz10@hotmail.com>
2023-02-13 13:58:41 -0300

    Create cli_clone

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     env: {
       hideCredentials: true,
       requestMode: true,
-      snapshotOnly : true      
+      snapshotOnly : true,
     }
   },
   fixturesFolder: false,
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,8 +15,10 @@
 
 // Import commands.js using ES2015 syntax:
 import 'cypress-plugin-api'
+import './cli_commands'
 import './gui_commands'
 import './api_commands'
+
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
